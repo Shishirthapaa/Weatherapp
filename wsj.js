41661df4e00d06a371ab0a3cfca4594dca5004bc
Shishir Thapa <128440154+Shishirthapaa@users.noreply.github.com>
2024-03-27 17:49:30 +0545

    Update wsj.js

--- wsj.js
+++ wsj.js
@@ -8,7 +8,7 @@
       
        else{
       
-       fetch("http://localhost/shishir/ptp2.php")
+       fetch("https://github.com/Shishirthapaa/Weatherapp/blob/main/ptp2.php")
        // Convert response string to json object
        .then((Response) => Response.json())
        .then((Response) => {
